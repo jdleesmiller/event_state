d2facd1f7857bbd6b045e18512dcb525b8aa9b9c
John Lees-Miller <jdleesmiller@gmail.com>
2012-01-23 17:47:00 +0000

    bump

--- lib/event_state/version.rb
+++ lib/event_state/version.rb
@@ -2,9 +2,9 @@ module EventState
   # Major version number.
   VERSION_MAJOR = 0
   # Minor version number.
-  VERSION_MINOR = 0
+  VERSION_MINOR = 1
   # Patch number.
-  VERSION_PATCH = 1
+  VERSION_PATCH = 0
   # Version string.
   VERSION = [VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH].join('.')
 end
